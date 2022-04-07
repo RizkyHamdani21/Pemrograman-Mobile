a9d7a68750314d4a78950fa12a98edb4c5ba1296
RizkyHamdani21 <74725772+RizkyHamdani21@users.noreply.github.com>
2022-04-07 23:42:45 +0800

    Add files via upload

---
+++ posttest1/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+extension ColorExtension on String {
+  toColor() {
+    var hexColor = this.replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    }
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var lebar = MediaQuery.of(context).size.width;
+    var tinggi = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("Posttest 1 Rizky Hamdani"))),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.blue,
+            Colors.red,
+          ],
+        )),
+        width: lebar,
+        height: tinggi,
+        child: Center(
+            child: Text(
+          "Rizky  Hamdani\n   2009106089  ",
+          style: TextStyle(
+            backgroundColor: "6BCB77".toColor(),
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        )),
+      ),
+    );
+  }
+}
